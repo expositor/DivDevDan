45eb4ab24db3d98f49979d34e5b81929244c0084
Meep <seishistuff@gmail.com>
2016-04-04 22:05:43 +0200

    I messed around a little.

---
+++ Assets/Moop.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moop : MonoBehaviour
+{
+    string myName;
+    float speed = 10;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Debug.Log("Moop: " + myName);
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+        float distanceh = speed * Time.deltaTime * Input.GetAxis("Horizontal");
+        float distancev = speed * Time.deltaTime * Input.GetAxis("Vertical");
+	    if (Input.GetKey("space"))
+        {
+            transform.Rotate(distanceh * Input.GetAxis("Horizontal"), 0, 0);
+            transform.Rotate(0, distancev * Input.GetAxis("Vertical"), 0);
+        }
+        else
+        {
+            transform.Translate(distanceh, 0, 0);
+            transform.Translate(0, distancev, 0);
+        }
+	}
+}
