4ec5815a2988766b07a481432991e68577fe7752
Meep <seishistuff@gmail.com>
2016-04-05 22:13:12 +0200

    double jump and controller buttons

--- Assets/scripts/Moop.cs
+++ Assets/scripts/Moop.cs
@@ -5,43 +5,44 @@ public class Moop : MonoBehaviour
 {
     public Rigidbody rb;
     public float speed;
+    private int jump;
+    private bool button;
 
 	void Start ()
 	{
         rb = GetComponent<Rigidbody>();
 
         speed = 0.1F;
+        jump = 2;
 	}
 
 	void Update ()
 	{
 	    if (Input.GetAxis("Horizontal") != 0)
         {
-            rb.MovePosition(rb.position + new Vector3(Mathf.Sign(Input.GetAxis("Horizontal")) * speed, 0, 0) );
+            rb.MovePosition(rb.position + new Vector3(Input.GetAxis("Horizontal") * speed, 0, 0) );
         }
 
 	    if (Input.GetAxis("Vertical") != 0)
         {
-            rb.MovePosition(rb.position + new Vector3(0, Mathf.Sign(Input.GetAxis("Vertical")) * speed, 0) );
+            rb.MovePosition(rb.position + new Vector3(0, 0, Input.GetAxis("Vertical") * speed) );
         }
-	    // if (Input.GetAxis("Horizontal") != 0 & rb.velocity.x == 0)
-        // {
-        //     rb.AddForce(speed * Mathf.Sign(Input.GetAxis("Horizontal")),0,0);
-        // }
-        // else if (rb.velocity.x != 0)
-        // {
-        //     rb.AddForce(-speed * 0.5F * Mathf.Sign(rb.velocity.x), 0, 0);
-        // }
-        //
-        // if (Input.GetAxis("Vertical") != 0 & rb.velocity.y < 10)
-        // {
-        //     rb.AddForce(0,speed * Mathf.Sign(Input.GetAxis("Vertical")),0);
-        // }
-        // else if (rb.velocity.y != 0)
-        // {
-        //     rb.AddForce(0, -speed * Mathf.Sign(rb.velocity.y), 0);
-        // }
 
+	    if (Input.GetButtonDown("Jump") & button == false)
+        {
+            button = true;
+            if (jump > 0) { rb.AddForce(0, 600, 0); jump--; }
+        }
+        else if (Input.GetButtonUp("Jump") & button == true)
+        {
+            button = false;
+        }
 	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag.Equals("Jumpable")) { jump = 2; }
+    }
 
 }
