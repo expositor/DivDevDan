d26b6d817e75b9259050cc6dd852a8e4ea987f14
Meep <seishistuff@gmail.com>
2016-04-06 13:10:23 +0200

    changed game resolution to 1920x1080, but found a hacky way to still have proper pixel grid alignment FOR REAL THIS TIME, remade title screen, added sword spinny animation, general cleanup

---
+++ Assets/scripts/rotatingsword.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotatingsword : MonoBehaviour
+{
+
+	void Start ()
+	{
+	}
+	void Update ()
+	{
+        transform.Rotate(0,0,0.03F);
+	}
+}
