28d4f1450c548c2bb11c166f918c79fe2a2d0ca9
Meep <seishistuff@gmail.com>
2016-04-12 23:27:54 +0200

    made the tilesystem work

--- Assets/Resources/Scripts/tilemap.cs
+++ Assets/Resources/Scripts/tilemap.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class tilemap : MonoBehaviour
@@ -9,9 +10,6 @@ public class tilemap : MonoBehaviour
     public int xGrid, yGrid;
     public int xRes, yRes;
 
-    private Vector3[] vertices;
-    private Vector2[] uv;
-    private Color32[] colors;
 
 	void Start ()
 	{
@@ -23,10 +21,27 @@ public class tilemap : MonoBehaviour
 
 	private void Awake()
     {
+
         Generate();
+
+    }
+
+    struct Tile
+    {
+
+        public int x;
+        public int y;
+
+        public Tile(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+
     }
 
     private Mesh mesh;
+    private Vector2[] uv;
     private void Generate()
     {
         xSize = xRes / xGrid;
@@ -35,56 +50,119 @@ public class tilemap : MonoBehaviour
         GetComponent<MeshFilter>().mesh = mesh = new Mesh();
         mesh.name = "Moop";
 
-        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
-        uv = new Vector2[vertices.Length];
 
-        for (int i = 0, y = 0; y <= ySize; y++)
+        Vector3[] vertices = new Vector3[xSize * ySize * 4];
+        uv = new Vector2[xSize * ySize * 4];
+        int[] triangles = new int[xSize * ySize * 6];
+
+        Tile[,] tiles = new Tile[,]
         {
-            for (int x = 0; x <= xSize; x++, i++)
+            {new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1)},
+            {new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1)},
+            {new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1)},
+            {new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1)},
+            {new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1)},
+            {new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1)},
+            {new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1)},
+            {new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(0,1),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1)},
+            {new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(0,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1)},
+            {new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1)},
+            {new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1)},
+            {new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1)},
+            {new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(0,1),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1)},
+            {new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(0,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1)},
+            {new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(0,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(0, 0),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1)},
+            {new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1)},
+            {new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1)},
+            {new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1),new Tile(1,0),new Tile(1, 1)}
+        };
+
+        Debug.Log("Moop: " + tiles.Length);
+
+        for (int i = 0, ti = 0, y = 0; y < ySize; y++)
+        {
+
+            for (int x = 0; x < xSize; x++, i+=4, ti+=6)
             {
-                vertices[i] = new Vector3(x*xGrid-xRes/2, y*yGrid-yRes/2, 0);
-                uv[i] = new Vector2((float)x, (float)y);
+
+                vertices[i] = new Vector3(x*xGrid-xRes/2, y*yGrid-yRes/2);
+                vertices[i+1] = new Vector3(x*xGrid-xRes/2, (y+1)*yGrid-yRes/2);
+                vertices[i+2] = new Vector3((x+1)*xGrid-xRes/2, (y+1)*yGrid-yRes/2);
+                vertices[i+3] = new Vector3((x+1)*xGrid-xRes/2, y*yGrid-yRes/2);
+                Debug.Log("x: " + tiles[y,x].x + "/ y: " + tiles[y,x].y);
+                uv[i] = new Vector2( ( tiles[y,x].x ) * 0.055555555F, ( tiles[y,x].y ) * 0.055555555F);
+                uv[i+1] = new Vector2( ( tiles[y,x].x ) * 0.055555555F, ( tiles[y,x].y + 1 ) * 0.055555555F);
+                uv[i+2] = new Vector2( ( tiles[y,x].x + 1 ) * 0.055555555F, ( tiles[y,x].y + 1 ) * 0.055555555F);
+                uv[i+3] = new Vector2( ( tiles[y,x].x + 1 ) * 0.055555555F, ( tiles[y,x].y ) * 0.055555555F);
+                // uv[i] = new Vector2((float)x/xSize, (float)y/ySize);
+                // uv[i+1] = new Vector2((float)x/xSize, (float)(y+1)/ySize);
+                // uv[i+2] = new Vector2((float)(x+1)/xSize, (float)(y+1)/ySize);
+                // uv[i+3] = new Vector2((float)(x+1)/xSize, (float)y/ySize);
+                triangles[ti] = i;
+                triangles[ti+1] = i+1;
+                triangles[ti+2] = i+2;
+                triangles[ti+3] = i;
+                triangles[ti+4] = i+2;
+                triangles[ti+5] = i+3;
+
             }
         }
 
-        int[] triangles = new int[xSize * ySize * 12];
-
-        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, ti += 6, vi++)
-        {
-
-            for (int x = 0; x < xSize; x++, ti += 6, vi++)
-            {
-                triangles[ti] = vi;
-                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
-                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
-                triangles[ti + 5] = vi + xSize + 2;
-            }
-
-        }
-
-        // colors = new Color32[(xSize + 1) * (ySize + 1)];
-        // for (int i = 0, y = 0; y <= ySize; y++, i += xSize+1)
+        // int[] triangles = new int[xSize * ySize * 6];
+        //
+        // for (int 
+        // for (int ti = 0, vi = 0, y = 0; y < ySize; y++, ti += 6, vi++)
         // {
-        //     for (int x = 0; x <= xSize; x++)
+        //
+        //     for (int x = 0; x < xSize; x++, ti += 6, vi++)
         //     {
-        //         colors[i + x] = new Color32((byte)(255/(xSize+1)*x),(byte)(255/(ySize+1)*y),0,255);
+        //         triangles[ti] = vi;
+        //         triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+        //         triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+        //         triangles[ti + 5] = vi + xSize + 2;
         //     }
+        //
         // }
         //
-        // for (int i = 0; i < xSize; i++)
-        // {
-        //     colors[i] = colors[i+1] = colors[i+xSize+1] = colors[i+xSize+2] = new Color32((byte)(i * 255/xSize), 0, 0, 255);
-        // }
-
-        Texture2D tex = (Texture2D)Resources.Load("Graphics/Bulbingsworth");
-
-        gameObject.GetComponent<Renderer>().material.mainTexture = tex;
-
 
         mesh.vertices = vertices;
         mesh.uv = uv;
         mesh.triangles = triangles;
-        mesh.colors32 = colors;
+
+        LoadThing();
+    }
+
+
+    private void LoadThing()
+    {
+        // uv = new Vector2[vertices.Length];
+        //
+        Texture2D tm = (Texture2D)Resources.Load("Graphics/TM1");
+        //
+        // List<int> mapping = new List<int>();
+        // mapping.Add(2);
+        // mapping.Add(1);
+        //
+        // // OMG, LOTS TO DO HERE
+        // // Step 1: Figure out a way how to compartmentalize tiles from their respective vertices and UV's !!
+        // //
+        // // for (int j = 0; j <= mapping.Length/xSize; j++)
+        // // {
+        // //
+        // //     for (int i = 0; i < xSize & i < mapping.Length; i++)
+        // //     {
+        // //         if (mapping[j+i] == null) { break; }
+        // //         uv[j+i] = new Vector2(mapping[j+i] * , );
+        // //     }
+        // //
+        // // }
+        //
+        // uv[0] = new Vector2((float)(1/32), 0);
+        // uv[1] = new Vector2((float)(1/32), (float)(1/32));
+        // uv[2] = new Vector2((float)(2*1/32), (float)(1/32));
+        // uv[3] = new Vector2((float)(2*1/32), 0);
+        // mesh.uv = uv;
+        gameObject.GetComponent<Renderer>().material.mainTexture = tm;
 
     }
 
