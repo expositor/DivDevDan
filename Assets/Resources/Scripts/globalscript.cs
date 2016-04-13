faf3a578dafedcdbf7a749aa96ac30ddecfc26e7
Meep <seishistuff@gmail.com>
2016-04-14 00:35:24 +0200

    added globalscript object that sticks around in any scene and may be used for global scripting needs (for example, you may press escape to switch back to the title screen now. If you are in the title screen it will quit the game, but that only works in the built version)

---
+++ Assets/Resources/Scripts/globalscript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class globalscript : MonoBehaviour
+{
+    static globalscript instance;
+
+    public static globalscript Instance
+    {
+        get
+        {
+            if (instance == null) { instance = FindObjectOfType<globalscript>(); }
+            if (instance == null)
+            {
+                GameObject obj = new GameObject("Globalscript");
+                obj.hideFlags = HideFlags.HideAndDontSave;
+                instance = obj.AddComponent<globalscript>();
+            }
+
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+
+            Destroy(this.gameObject);
+            return;
+
+        }
+
+        instance = this;
+        DontDestroyOnLoad( this.gameObject );
+    }
+
+	void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.Equals(Event.KeyboardEvent("escape")))
+        {
+
+            if (SceneManager.GetActiveScene().name.Equals("titlescreenNEW2")) { Application.Quit(); }
+            else { SceneManager.LoadScene("titlescreenNEW2"); }
+
+        }
+
+    }
+
+}
